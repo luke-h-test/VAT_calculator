d2e2af5612e7b01df04180aafd8f92e9eb64d625
Luke <purelyfortoontown2@gmail.com>
2023-10-18 13:58:49 +0100

    Asking user input functionality added

--- src/main/java/com/lbg/cohort4/Main.java
+++ src/main/java/com/lbg/cohort4/Main.java
@@ -1,19 +1,32 @@
 package com.lbg.cohort4;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        float itemcost;
+        float VATrate;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+
+        priceprompt();
+        itemcost = sc.nextFloat();
+        VATprompt();
+        VATrate = sc.nextFloat();
+
+
+    }
+
+    static private void priceprompt()
+    {
+        System.out.println("Please enter cost of item before VAT: ");
+    }
+
+    static private void VATprompt()
+    {
+        System.out.println("Please enter the VAT rate %: ");
     }
 }
